2acaeb263e4f06d16b43768ce7dcdcd9508037ac
ARJUN SHIVA KATHIRAVAN <59244027+ARJUNSHIVA23@users.noreply.github.com>
2024-07-08 19:31:07 +0530

    Update filehandling.c

--- filehandling.c
+++ filehandling.c
@@ -3,7 +3,7 @@ void main()
 {
     FILE *fp;
     char ch;
-    fp=fopen("C:/Users/Arjun Shiva PC/Desktop/New folder/1.Arithmetic operations.txt","r");
+    fp=fopen("MYFILE.txt","r");
     while(1)
     {
         ch=fgetc(fp);
